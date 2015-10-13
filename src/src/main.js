f7a2baf512134e0dd81120ee96a68b91f80af4a9
nalinc <nalin.chhibber@gmail.com>
2015-10-13 18:11:01 +0530

    enable user to drag nudge-messenger

--- src/src/main.js
+++ src/src/main.js
@@ -443,11 +443,50 @@
       
   }
 
+  var selected = null, // Object of the element to be moved
+      x_pos = 0, y_pos = 0, // Stores x & y coordinates of the mouse pointer
+      x_elem = 0, y_elem = 0; // Stores top, left values (edge) of the element
+
+  // Will be called when user starts dragging an element
+  function _drag_init(elem) {
+      // Store the object of the element which needs to be moved
+      selected = elem;
+      x_elem = x_pos - selected.offsetLeft;
+      y_elem = y_pos - selected.offsetTop;
+  }
+
+  // Will be called when user dragging an element
+  function _move_elem(e) {
+      x_pos = document.all ? window.event.clientX : e.pageX;
+      y_pos = document.all ? window.event.clientY : e.pageY;
+      if (selected !== null) {
+          selected.style.left = (x_pos - x_elem) + 'px';
+          selected.style.top = (y_pos - y_elem) + 'px';
+      }
+  }
+
+  // Destroy the object when we are done
+  function _destroy() {
+      selected = null;
+  }
+
+
+
+  document.onmousemove = _move_elem;
+  document.onmouseup = _destroy;
+
+
   init = function(){
 
               createDOMstructure();
               addStyleSheets();
 
+              // Bind the functions...
+/*              document.getElementById('wrapper').onmousedown = function () {
+                  _drag_init(this);
+                  return false;
+              };
+*/
               var messenger = new nudge_messenger.Messenger();
               var buildHTML = new nudge_messenger.BuildHTML();
 
